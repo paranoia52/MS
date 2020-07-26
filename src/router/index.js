b6183323a49220f195abe9d2d11e5b474a9927a6
paranoia52 <57613015+paranoia52@users.noreply.github.com>
2020-07-26 12:09:57 +0800

    基本路由配置
    1

--- src/router/index.js
+++ src/router/index.js
@@ -10,8 +10,20 @@ const routes = [
   },
   {
     path: '/index',
-    components: () => import('@/views/index/Index.vue')
-  }
+    component: () => import('@/views/index/Index.vue')
+  },
+  {
+    path: '/music',
+    component: () => import('@/views/music/Music.vue')
+  },
+  {
+    path: '/wallpaper',
+    component: () => import('@/views/wallpaper/WallPaper.vue')
+  },
+  {
+    path: '/book',
+    component: () => import('@/views/book/Book.vue')
+  },
 ]
 
 const router = new VueRouter({
@@ -19,5 +31,11 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+//以下代码解决路由地址重复点击的报错问题
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 export default router
--- src/views/wallpaper/WallPaper.vue
+++ src/views/wallpaper/WallPaper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="index">index</div>
+  <div class="wallpaper">wallpaper</div>
 </template>
 
 <script>
